05562cc81ab9462e1922b3cfbe65042ecfd3a686
YOU <khy960101@gamil.com>
2022-09-23 14:43:31 +0900

    DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("제발 좀 되라");
-console.log("제발 좀 되라2");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id : id.value,
+    psword : psword.value,
+  }
+  console.log(req);
+}
