d29d827fa0d33ac5fc663c3fe1e61b3870b616b5
YOU <khy960101@gamil.com>
2022-09-23 16:52:25 +0900

    body-parser를 이용한 입력값 불러오기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,16 +1,24 @@
 "use strict";
 
-const hello = (req, res) => {
-  res.render("home/index");
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
-};
+const process = {
+  login: (req, res) => {
+    console.log(req.body);
+  },
+}
+
 
 
 // module을 오브젝트 형태로 내보내는데 value 없이 key만 보내면 hello : hello, login : login 이런 형태로 나가는 거라고 볼 수 있다.
 module.exports = {
-  hello,
-  login,
+  output,
+  process,
 };
