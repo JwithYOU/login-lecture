76428f87397bd026429e61fa49dd2949454903c3
YOU <khy960101@gamil.com>
2022-09-24 17:47:05 +0900

    회원가입 요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+  userName = document.querySelector("#user-name"),
+  psword = document.querySelector("#psword"),
+  confirmPsword = document.querySelector("#confirm-psword"),
+  registerBtn = document.querySelector("#button");
+
+registerBtn.addEventListener("click", register);
+
+function register() {
+  const req = {
+    id: id.value,
+    userName: userName.value,
+    psword: psword.value,
+    confirmPsword: confirmPsword.value,
+  };
+
+  console.log(req);
+  
+  // fetch("register", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(req),
+  // })
+  // .then((res) => res.json())
+  // .then((res) => {
+  //   if(res.success) {
+  //     location.href = "/";
+  //   } else {
+  //     alert(res.msg);
+  //   }
+  // })
+  // .catch((err) => {
+  //   console.error(new Error("로그인 중에 에러 발생")) // new Error를 통해서 에러를 만들어 줄 수 있음
+  // });
+}
