6d7f2224f00ca0cb7a3fc9be7ad67de8ced5d528
YOU <khy960101@gamil.com>
2022-09-26 21:38:45 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,23 @@
 "use strict";
 
+const { userInfo } = require('os');
+
+const fs = require("fs").promises; // promises를 붙여줌으로써 유지보수 및 읽기에 좋다 
+
 class UserStorage { // class안에는 따로 변수명을 선언할 필요 x
-  static #users = {
-    id: ["카카오", "네이버", "쿠팡"],
-    name: ["김코딩", "박해커", "이테크"],
-    psword: ["1234", "1234", "1234"],
+  static #getUserInfo(data, id) { // private한 변수나 메서드는 최상단에 올려준다
+    const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users); // => [id, psword, name];
+        const userInfo = userKeys.reduce((newUser, info) => {
+          newUser[info] = users[info][idx];
+          return newUser;
+        }, {});
+        return userInfo;
   }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((arr, cur) => {
       if(users.hasOwnProperty(cur)) {
         arr[cur] = users[cur];
@@ -19,18 +28,17 @@ class UserStorage { // class안에는 따로 변수명을 선언할 필요 x
   }
 
   static getUserInfo(id) {
-    const users = this.#users; // 전체 아이디, 비밀번호 불러옴
-    const idx = users.id.indexOf(id);
-    const userKeys = Object.keys(users);
-    const userInfo = userKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-  }, {});
-    return userInfo;
+    // const users = this.#users; // 전체 아이디, 비밀번호 불러옴
+    return fs 
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error()); // err => console.log(err)
   }
-
+  
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
